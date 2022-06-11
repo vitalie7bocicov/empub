2d71d193fa3dc7dd596541fffce32b52d1de184a
eduardsoltan <eduardsoltan@gmail.com>
2022-06-11 15:40:06 +0200

    delete everthing

--- emails/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require './application/core/App.php';
-
-ini_set('display_errors', 1);
-error_reporting(E_ERROR | E_PARSE);
-
-
-$app = new App();
--- login/application/core/Controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Class Controller {
-
-    public static function view($view, $data = []) {
-        require_once '../application/views/'. $view . '.php';
-    }
-}
