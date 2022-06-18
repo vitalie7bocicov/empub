a07f2a911bc1cb0646138f60bc3cab16bbc30911
TiberiuBadelita <79211116+TiberiuBadelita@users.noreply.github.com>
2022-06-18 21:53:52 +0300

    EXPORT Users as CSV

--- empub/application/scripts/dashboard.js
+++ empub/application/scripts/dashboard.js
@@ -11,6 +11,46 @@ var btn = document.getElementById("btn");
 
 
 var span = document.getElementsByClassName("close")[0];
+    
+var csv = document.getElementById("csv");
+
+csv.addEventListener('click', ()=>{
+    const authToken = `Bearer ${localStorage.getItem('accessToken')}`;
+    let myHeaders = new Headers();
+    myHeaders.append('Authorization', authToken);
+    let request = new Request(`http://localhost/TehnologiiWeb/users/users`, {
+        method: 'GET',
+        headers: myHeaders
+    });
+    fetch(request)
+        .then(res => {
+            if(res.status != 200) {
+                checkStatus(res.status);
+                throw new TypeError (`Response with code ${res.status}`);
+            }
+            const contentType = res.headers.get('Content-Type');
+            if(contentType && contentType.includes('application/json')) {
+                return res.json();
+            }
+            throw new TypeError ('Response got is not in correct format');
+        })
+        .then(data => {
+                 console.log(data);
+                let csvContent = "data:text/csv;charset=utf-8,";
+
+                data.forEach(function(rowArray) {
+                    let row = Object.values(rowArray).join(",");
+                    csvContent += row + "\r\n";
+                });
+
+                var encodedUri = encodeURI(csvContent);
+                window.open(encodedUri);
+            
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});    
 
 
 btn.onclick = function() {
